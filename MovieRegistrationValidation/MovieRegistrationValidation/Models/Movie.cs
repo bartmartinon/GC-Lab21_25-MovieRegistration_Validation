1ff806bab71ac62f91c5f6651a8382d56a54f21e
Bart Martinon <barthelemy.martinon@gmail.com>
2020-11-13 16:36:52 -0500

    Added Razor validation to Model and MovieRegistrationForm

--- MovieRegistrationValidation/MovieRegistrationValidation/Models/Movie.cs
+++ MovieRegistrationValidation/MovieRegistrationValidation/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,16 @@ namespace MovieRegistrationValidation.Models
     public class Movie
     {
         // Fields/Properties
+        [Required(ErrorMessage = " Please enter an ID.")]
+        [StringLength(10, ErrorMessage = "ID is too long.")]
+        [RegularExpression(@"[0-9A-F]+", ErrorMessage = "Invalid ID format.")]
         public string ID { get; set; }
+        [Required(ErrorMessage = "Please enter a Title.")]
+        [StringLength(50, ErrorMessage = "Title is too long, please keep below 50 characters.")]
         public string Title { get; set; }
         public string Genre { get; set; }
         public int Year { get; set; } = 1900;
         public List<string> Actors { get; set; } = new List<string>();
         public List<string> Directors { get; set; } = new List<string>();
-}
+    }
 }
